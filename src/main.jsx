773654330930333970be61c3affb957d43e87ead
bringthepunters <155452570+bringthepunters@users.noreply.github.com>
2025-02-10 12:46:36 +1100

    it deploys, and the page runs - but "generate images" fails

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import InstagramGallery from './instagramgallery.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <InstagramGallery />
