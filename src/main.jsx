15a6bd1f37e2bdb3cf960024932381d4c9e92da9
bringthepunters <155452570+bringthepunters@users.noreply.github.com>
2025-02-10 12:39:55 +1100

    fixed path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import InstagramGallery from './InstagramGallery.jsx'
+import InstagramGallery from './instagamgallery.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
