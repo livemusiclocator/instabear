3ce7f9067f7bafd6fcb6664d65ba53bd852d1676
Mark Ryall <mark@fresho.com>
2025-08-06 19:08:12 +1000

    base dir no longer required

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/instabear/',  // Relative path for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
